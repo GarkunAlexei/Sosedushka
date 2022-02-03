0a81026d5f7332c134d4352b942d030b0bc556fd
Andrew <andrey.kitaev55@gmail.com>
2022-02-03 19:58:49 +0300

    component create announcement

--- client/src/components/ContentS/ContentS.jsx
+++ client/src/components/ContentS/ContentS.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Layout } from 'antd';
 import style from './style.module.css'
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import AnnouncementCreate from '../AnnouncementCreate/AnnouncementCreate';
 const { Content } = Layout;
 
 function ContentS() {
@@ -9,8 +10,8 @@ function ContentS() {
     <Content style={{ padding: '0 400px' }}>
       <div className={style.site_layout_content}>
       <Routes>
+        <Route path="/createannouncement" element={<AnnouncementCreate />} />
         {/* <Route path="/" element={<MainPage />} />
-        <Route path="/" element={<MainPage />} />
         <Route path="/" element={<MainPage />} />
         <Route path="/" element={<MainPage />} />
         <Route path="/" element={<MainPage />} />
