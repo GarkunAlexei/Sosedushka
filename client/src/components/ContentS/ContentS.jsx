9cb658bd92e95eb7dba5b4d3aee54ce572b912ac
Evgeniy <JenyaUtkin90@yandex.ru>
2022-02-04 17:12:27 +0300

    fixUser

--- client/src/components/ContentS/ContentS.jsx
+++ client/src/components/ContentS/ContentS.jsx
@@ -11,6 +11,7 @@ import UserWrapper from '../UserWrapper/UserWrapper';
 import { Routes, Route } from 'react-router-dom';
 import AuthUser from '../ProtectedAuth/AuthUser';
 import ProtectedAuth from '../ProtectedAuth/ProtectedAuth';
+import UserPage from '../UserPage/UserPage';
 
 const { Content } = Layout;
 
@@ -42,9 +43,9 @@ function ContentS() {
             <FormAccount />
           </ProtectedAuth>      
           } />
-          <Route path="/createannouncement" element={
+          <Route path="/profil" element={
           <ProtectedAuth>
-            <AnnouncementCreate />
+            <UserPage />
           </ProtectedAuth>
           } />
           <Route path="/extra" element={
