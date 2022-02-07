11ab810c02650dff689b07b15558d8b6ea008188
Alexei.G <Bigud007@mail.ru>
2022-02-07 10:55:00 +0300

    add Interest Redux State

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { interestReducer } from './interestReducer';
 import { rolesReducer } from './rolesReducer';
 import { userReducer } from './userReducer';
 
 export const rootReducer = combineReducers({
     user: userReducer,
     roles: rolesReducer,
+    interest: interestReducer,
 });
--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ var FileStore = require('session-file-store')(session);
 
 const userRouter = require('./routes/userRouter');
 const rolesRouter = require('./routes/rolesRouter');
+const interestRouter = require('./routes/interestRouter');
 
 const PORT = process.env.PORT ?? 3001;
 const app = express();
@@ -32,7 +33,8 @@ app.use(
 );
 
 app.use('/user', userRouter);
-app.use('/roles', rolesRouter)
+app.use('/roles', rolesRouter);
+app.use('/interest', interestRouter);
 
 app.listen(PORT, () => {
     console.log('server start on port ', PORT)
