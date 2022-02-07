2b5f1e5b019bb938136e566aad62b9fe676b9586
Andrew <andrey.kitaev55@gmail.com>
2022-02-07 18:22:01 +0300

    Release 2.0

--- client/src/components/AnnouncementCreate/AnnouncementCreate.jsx
+++ client/src/components/AnnouncementCreate/AnnouncementCreate.jsx
@@ -8,21 +8,39 @@ import {
 } from 'antd';
 import { Upload } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
-import { Typography } from 'antd';
+import { Typography, Progress, Modal } from 'antd';
 import style from './style.module.css'
 import YMapComponent from '../YMap/CreatePoint/YMapComponent';
 import { useEffect } from 'react';
-import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { addAd } from '../../redux/actions/adAC';
+import { storage } from '../../firebase/firebase';
+import { getDownloadURL, ref, uploadBytesResumable } from '@firebase/storage';
 
 const { Title } = Typography;
 
 function AnnouncementCreate() {
 
-  const [input, setInput] = useState({ name: '', coords: null, cost: '', description: '', address: '' })
+  const [progress, setProgress] = useState();
+  const [image, setImage] = useState(null)
+  const [input, setInput] = useState({ name: '', coords: null, cost: '', description: '', address: '', img: '' })
 
-  console.log('...INPUT ====>', {...input});
+  console.log('...INPUT ====>', { ...input });
+  console.log('IMAGE ====>', image)
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   const dispatch = useDispatch();
 
@@ -54,10 +72,40 @@ function AnnouncementCreate() {
     setInput(prev => ({ ...prev, [e.target.name]: e.target.value }))
   }
 
+  const handleChange = (e) => {
+    console.log('TARGET ====>', e.target.files);
+    if (e.target.files[0]) {
+      setImage(e.target.files[0])
+    }
+  }
+
   const submitHandler = (e) => {
     e.preventDefault();
+    const file = e.target[0].files[0]
+    console.log(file);
+    uploadFiles(file)
     dispatch(addAd(input))
     // setInput({})
+  }
+
+  const uploadFiles = () => {
+
+    console.log('uploadFiles');
+    const storageRef = ref(storage, `/files/${image.name}`)
+    const uploadTask = uploadBytesResumable(storageRef, image)
+
+    uploadTask.on('state_changed', (snaphot) => {
+      const prog = Math.round(
+        (snaphot.bytesTransferred / snaphot.totalBytes) * 100
+      );
+      setProgress(prog);
+
+    }, (err) => console.log(err),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then(url => setInput(prev => ({ ...prev, img: url })))
+      }
+    )
   }
 
 
@@ -72,6 +120,7 @@ function AnnouncementCreate() {
           span: 12,
         }}
         layout="horizontal"
+        onSubmit={submitHandler}
       >
         <Form.Item label="Название:" onChange={inputHandler}>
           <Input name="name" value={input.name} />
@@ -93,15 +142,30 @@ function AnnouncementCreate() {
           <Input.TextArea name="description" value={input.description} />
         </Form.Item>
         <Form.Item label="Фото:">
-          <Upload {...props}>
-            <Button icon={<UploadOutlined />}>Загрузить</Button>
-          </Upload>
+
+
+          <Button type="primary" onClick={showModal}>
+            Загрузить фото
+          </Button> <span></span>
+
+          <Modal title="Загрузка фото" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            {/* <Upload type="file" name="img" onChange={handleChange}>
+              <Button>Выберите файлы</Button>
+            </Upload> */}
+            <input type="file" name="img" onChange={handleChange} /> <br />
+            <Button icon={<UploadOutlined />} type='submit' onClick={uploadFiles}>Загрузить</Button>
+            <Progress percent={progress} size="small" /> {progress} %
+          </Modal>
+          { progress ? 
+              <Progress percent={progress} steps={1} size="small" strokeColor="#52c41a" />:
+              <p></p>
+          }
         </Form.Item>
         <Form.Item label="Местоположение" >
-          <YMapComponent setInput={setInput}/>
+          <YMapComponent setInput={setInput} />
         </Form.Item>
-        <Form.Item label=" ">
-          <Button onClick={submitHandler} type="primary">Подать объявление</Button>
+        <Form.Item label={' '}>
+          <Button type="primary">Подать объявление</Button>
         </Form.Item>
       </Form>
     </>
