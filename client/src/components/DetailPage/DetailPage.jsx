64853477d526fe88626bc5e484b6c38be7078abf
Alexei.G <Bigud007@mail.ru>
2022-02-09 19:18:45 +0300

    PlusNoticeInterests

--- client/src/components/DetailPage/DetailPage.jsx
+++ client/src/components/DetailPage/DetailPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row, Col} from 'antd'
+import { Row, Col, Button } from 'antd'
 import { FlatDetail } from '../FlatDetail/FlatDetail';
 import { UserDetail } from '../UserDetail/UserDetail';
 import { useParams } from 'react-router-dom';
@@ -23,6 +23,7 @@ export const DetailPage = () => {
       }
   }, [])
 
+  console.log(userAnketa?.interests);
   // console.log('----------->', userAnketa);
   // console.log('----------->', userAnketa.note.Photos[0].photo);
   // console.log('----------->', userAnketa.user);
@@ -30,14 +31,16 @@ export const DetailPage = () => {
   return (
     <Row>
       {userAnketa &&
+      
       <>
+      {console.log(userAnketa)}
       <Col xs={12}>
         <FlatDetail 
         key={userAnketa.note.id} 
         address={userAnketa.note.address} 
         description={userAnketa.note.description}
         cost={userAnketa.note.cost}
-        img={userAnketa.note.Photos[0]?.photo}/>
+        img={userAnketa.note.Photos[0].photo}/>
       </Col>
       <Col xs={12}>
         <UserDetail
@@ -45,7 +48,14 @@ export const DetailPage = () => {
           phone={userAnketa.user.phone}
           img={userAnketa.user.photo}
           about_me={userAnketa.user.about_me}
+          name={userAnketa.user.name}
         />
+        </Col>
+        <Col xs={12}>
+        </Col>
+        <Col xs={12}>
+        <h4>{userAnketa.user.name} о своих интересах:</h4>
+            <p>{userAnketa.interests.map(el => <Button>{el.interest}</Button>)}</p>
         </Col>
         </>
       }
--- client/src/components/FlatDetail/FlatDetail.jsx
+++ client/src/components/FlatDetail/FlatDetail.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import {Image, Typography, Space} from 'antd'
+import { Image, Typography, Space, Carousel } from 'antd'
 
 export const FlatDetail = ({address, description, cost, img}) => {
   const{Title, Text} = Typography;
+
+  const contentStyle = {
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
   return (
     <>
     <Title level={3}>5-комн.квартира, 200,4 м²</Title>
